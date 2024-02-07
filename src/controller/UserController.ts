c6051f0398478f68e80af537f90f3b025ac1ea47
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-02-07 23:17:28 +0530

    create UserController ts

---
+++ src/controller/UserController.ts
@@ -0,0 +1,9 @@
+
+
+const register=(req:any,res:any)=>{
+    return res.status(200).json({message:"ok"})
+}
+
+module.exports={
+    register
+}
--- src/model/UserModel.ts
+++ src/model/UserModel.ts
@@ -1,11 +1,13 @@
-import mongoose from "mongoose";
+import mongoose, {Document,Schema,model} from "mongoose";
+import *  as SchemaType from "../types/SchemaTypes";
 
 
-const UserModel=new mongoose.Schema({
+const UserModel=new mongoose.Schema<SchemaType.IUser>({
     fullName:{type:String,require:true},
     email:{type:String,require:true},
     password:{type:String,require:true},
     activeState:{type:Boolean,require:true}
 })
 
-module.exports=mongoose.model('user',UserModel)
+const UserMode=mongoose.model('user',UserModel);
+export default UserModel;
